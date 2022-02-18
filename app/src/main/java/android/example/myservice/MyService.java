92e2f920d43523dd4df51a3ac70d46c643236b7b
ashwin <ashwin.selvendran@cognizant.com>
2022-02-18 10:16:50 -0500

    Android services tutorial.

---
+++ app/src/main/java/android/example/myservice/MyService.java
@@ -0,0 +1,39 @@
+package android.example.myservice;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class MyService extends Service {
+
+    public static String TAG = MyService.class.getSimpleName();
+    public MyService() {
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.i(TAG,"service created");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        super.onStartCommand(intent, flags, startId);
+        Log.i(TAG, "received request to----"+ intent.getExtras().getString("url"));
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG,"service destroyed");
+    }
+
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        return null;
+    }
+}
